2aecb8f30557c5b10f3cf782369d940a26b405f1
danrigsby <danrigsby@gmail.com>
2015-06-23 21:32:56 -0400

    initial commit

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -54,3 +54,35 @@ Install-ChocolateyPinnedTaskBarItem "$env:programfiles\Evernote\Evernote\Evernot
 npm install -g gulp
 npm install -g grunt
 npm install -g mocha
+
+# Atom Plugins
+apm install sublime-style-column-selection
+apm install autocomplete-plus
+apm install autocomplete-paths
+apm install enhanced-tabs
+apm install file-icons
+apm install language-scala
+apm install minimap
+apm install open-recent
+apm install react
+apm install git-log
+apm install git-go
+apm install git-history
+apm install git-plus
+apm install git-projects
+apm install merge-conflicts
+apm install pigments
+apm install highlight-selected
+apm install project-manager
+apm install editorconfig
+apm install highlight-line
+apm install regex-railroad-diagram
+apm install git-blame
+apm install minimap-selection
+apm install trailing-spaces
+apm install minimap-color-highlight
+apm install minimap-git-diff
+
+# Git Setup
+git config --global user.email "danrigsby@hotmail.com"
+git config --global user.name "danrigsby"
