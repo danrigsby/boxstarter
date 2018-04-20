9071e3b1a12c2a7384a58f6cbbdbd181e491a855
Dan Rigsby <danrigsby@gmail.com>
2018-04-20 14:36:25 -0400

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -7,7 +7,6 @@ Set-ExplorerOptions - showHiddenFilesFolderDrives -showProtectedOSFIles -showFil
 Enable-RemoteDesktop
 
 # Dev Tools
-cinst -y atom
 cinst -y vscode
 cinst -y cmder
 cinst -y boot2docker
@@ -59,42 +58,3 @@ Install-WindowsUpdate -AcceptEula
 # NPM Programs
 npm install -g yo
 npm install -g mocha
-
-# Atom Plugins
-apm install autocomplete-modules
-apm install autocomplete-paths
-apm install atom-ternjs
-apm install docblockr
-apm install editorconfig
-apm install enhanced-tabs
-apm install find-and-till
-apm install file-icons
-apm install git-blame
-apm install git-history
-apm install git-log
-apm install git-plus
-apm install highlight-line
-apm install highlight-selected
-apm install language-scala
-apm install linter
-apm install linter-eslint
-apm install merge-conflicts
-apm install minimap
-apm install minimap-find-and-replace
-apm install minimap-git-diff
-apm install minimap-highlight-selection
-apm install minimap-linter
-apm install minimap-pigments
-apm install minimap-selection
-#apm install nuclide-installer
-apm install open-recent
-apm install pigments
-apm install react
-apm install regex-railroad-diagram
-apm install sort-lines
-apm install script
-apm install sublime-style-column-selection
-apm install sync-settings
-apm install atom-ternjs
-apm install zen
-apm install atom-react-native-autocomplete
